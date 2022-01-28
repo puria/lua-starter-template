5de43501fce28af17c0259b7624215a769c41678
Puria Nafisi Azizi <puria@dyne.org>
2022-01-28 17:36:33 +0100

    Fix CI errors

--- spec/test1_spec.lua
+++ spec/test1_spec.lua
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2022 2022 Puria Nafisi Azizi <puria@dyne.org>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
 local m = require("starter")
 
 describe("Busted unit testing framework", function()
